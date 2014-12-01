e54947816c04291c0df055d7b91e1f95b8fce921
Michael Feldstein <msfeldstein@gmail.com>
2014-12-01 15:46:48 -0800

    Start adding qr code

--- Framer Connect/AppDelegate.m
+++ Framer Connect/AppDelegate.m
@@ -7,12 +7,15 @@
 //
 
 #import "AppDelegate.h"
+#import "QRCodeGenerator.h"
 
 @interface AppDelegate () {
     NSNetService* _service;
 }
 @property NSStatusItem* statusItem;
 @property IBOutlet NSMenu* statusMenu;
+@property IBOutlet NSWindow* qrWindow;
+@property IBOutlet NSImageView* qrView;
 @end
 
 @implementation AppDelegate
@@ -23,14 +26,33 @@
     self.statusItem.menu = self.statusMenu;
     _service = [[NSNetService alloc] initWithDomain:@"" type:@"_framerconnect._tcp." name:@"Framer Connect Service" port:8000];
     [_service publish];
+    
+    self.qrView.layer.backgroundColor = [NSColor whiteColor].CGColor;
+    self.qrView.image = [QRCodeGenerator qrImageForString:[self getIPAddress] imageSize:self.qrView.bounds.size.width];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
 }
 
+- (IBAction)showQR:(id)sender {
+    [self.qrWindow makeKeyAndOrderFront:nil];
+}
+
 - (IBAction)quit:(id)sender {
     [NSApp terminate:nil];
+}
+
+-(NSString *)getIPAddress
+{
+    for (NSString* address in [[NSHost currentHost] addresses]) {
+        if ([address componentsSeparatedByString:@"."].count == 4) {
+            if (![address isEqualToString:@"127.0.0.1"]) {
+                return address;
+            }
+        }
+    }
+    return nil;
 }
 
 @end
